6bd4110248021deedc728a9f335132aa16a9e8ce
Tom <wonggamto@163.com>
2020-12-16 16:54:45 +0800

    statistics页面基本布局完成

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -5,13 +5,23 @@
 </template>
 
 <script lang="ts">
-    import Nav from '@/components/Nav.vue';
-    export default{
-        name:"Statistics",
-      components: {Nav}
-    }
+import Nav from '@/components/Nav.vue';
+
+export default {
+  name: 'Statistics',
+  components: {Nav}
+};
 </script>
 
 <style lang="scss" scoped>
-
+.nav-wrappers {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  > .content{
+    border: 1px solid red;
+    flex-grow: 1;
+    overflow: auto;
+  }
+}
 </style>
