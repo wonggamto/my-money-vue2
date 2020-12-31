d17468c28fa78ae79beb6570bf1775ea6ec52f55
Tom <wonggamto@163.com>
2020-12-31 14:43:26 +0800

    获取tag的id并且放到路径中

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -8,11 +8,23 @@
 import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
 import Layout from '../../money-vue2-2/src/components/Layout.vue';
+import {tagListModel} from '@/models/tagListModel';
+
 @Component({
   components: {Layout}
 })
 export default class EditLabel extends Vue {
-
+  created() {
+    const id = this.$route.params.id;
+    tagListModel.fetch();
+    const tags = tagListModel.data;
+    const tag = tags.filter(t => t.id === id)[0];
+    if(tag){
+      console.log(tag);
+    }else{
+      this.$router.replace('/404')
+    }
+  }
 }
 </script>
 <style lang="scss" scoped>
