e55f0fbbc25c3137ed0fe5b4cf0fdf14a0ca1d1b
Tom <wonggamto@163.com>
2021-01-31 11:33:50 +0800

    添加qrcode，保证用户用手机打开

--- src/main.ts
+++ src/main.ts
@@ -15,3 +15,15 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount('#app');
+
+if (document.documentElement.clientWidth > 500) {
+  window.alert('请输入用手机打开本页面，以保证浏览效果。');
+  const img = document.createElement('img');
+  img.src = '/qrcode.png';
+  img.style.position = 'fixed';
+  img.style.left = '50%';
+  img.style.top = '50%';
+  img.style.transform = 'translate(-50%,-50%)';
+  img.style.boxShadow = '0 0 10px rgba(0,0,0,0.25)';
+  document.body.appendChild(img);
+}
