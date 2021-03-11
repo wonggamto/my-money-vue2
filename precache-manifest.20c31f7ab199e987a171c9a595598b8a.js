131310d60a1b7559da3928036d84114f4d55ea43
Tom <wonggamto@163.com>
2021-03-11 23:50:39 +0800

    deploy

---
+++ precache-manifest.20c31f7ab199e987a171c9a595598b8a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b8045aa2e042002a50b",
+    "url": "/my-money-vue2/css/app.10e104a1.css"
+  },
+  {
+    "revision": "c5db207841817aeca0b6",
+    "url": "/my-money-vue2/css/chunk-vendors.a9eff9bf.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/my-money-vue2/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/my-money-vue2/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "d50a7d03fd21eb2e08c81ad8deac731d",
+    "url": "/my-money-vue2/index.html"
+  },
+  {
+    "revision": "0b8045aa2e042002a50b",
+    "url": "/my-money-vue2/js/app.c9123127.js"
+  },
+  {
+    "revision": "c5db207841817aeca0b6",
+    "url": "/my-money-vue2/js/chunk-vendors.b20f01bf.js"
+  },
+  {
+    "revision": "b9b5bc4499736e76a57495cd6f04a3c5",
+    "url": "/my-money-vue2/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/my-money-vue2/robots.txt"
+  }
+]);
